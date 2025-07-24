cff89cde7580a2949d3f2e0fd1ee813ef75f056e
Adiseshan Ramanan <adiseshanramanan@gmail.com>
2025-07-24 16:14:46 +0530

    endpoint-1 for greet

--- Math_CLI/microservice/cli_tool.js
+++ Math_CLI/microservice/cli_tool.js
@@ -2,6 +2,10 @@ const express= require('express');
 
 const app= express()
 
+app.get('/', function(req,res){
+    res.send("Welcome to the Math, Basic arithmetic operations CLI tool, buil using Node.js and Express.js");
+});
+
 app.listen(3000, ()=>{
     console.log("Math CLI tool is running on port 3000...");
 });
