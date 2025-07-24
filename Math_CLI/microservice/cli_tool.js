dbc080b7ee04270a6d3621f7d4273386412a3de5
Adiseshan Ramanan <adiseshanramanan@gmail.com>
2025-07-24 16:21:54 +0530

    did one endpoint and tested it

--- Math_CLI/microservice/cli_tool.js
+++ Math_CLI/microservice/cli_tool.js
@@ -3,7 +3,15 @@ const express= require('express');
 const app= express()
 
 app.get('/', function(req,res){
-    res.send("Welcome to the Math, Basic arithmetic operations CLI tool, buil using Node.js and Express.js");
+    res.send("Welcome to the Math, basic arithmetic operations CLI tool, built using Node.js and Express.js");
+});
+
+app.get('/sum/:firstArg/:secondArg', function(req,res){
+    const a=parseInt(req.params.firstArg);
+    const b= parseInt(req.params.secondArg);
+    res.json({
+        answer: a+b
+    });
 });
 
 app.listen(3000, ()=>{
